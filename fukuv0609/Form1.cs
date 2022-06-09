5c9d5f79cce47f27221d4d9e072346ae0897a8d9
syouzuiken <laybaekzhang@gmail.com>
2022-06-09 15:09:31 +0900

    Initial commit

---
+++ fukuv0609/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fukuv0609
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("章瑞謙");
+            timer1.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += int.Parse(textBox1.Text);
+            label1.Top += int.Parse(textBox2.Text);
+        }
+    }
+}
